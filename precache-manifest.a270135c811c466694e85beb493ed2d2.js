d1d6d56f38b98dbbde6569dd9ac4bb5371268e89
Reinel Fernando Puentes Moreno <rpuentesm@ssf.gov.co>
2023-02-22 13:40:01 -0500

    Updates

---
+++ precache-manifest.a270135c811c466694e85beb493ed2d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "94e6ab5dac44d9cc1091",
+    "url": "/TodosReact/static/css/main.e6bb3993.chunk.css"
+  },
+  {
+    "revision": "94e6ab5dac44d9cc1091",
+    "url": "/TodosReact/static/js/main.94e6ab5d.chunk.js"
+  },
+  {
+    "revision": "1daa8b14f4ed358548f7",
+    "url": "/TodosReact/static/js/1.1daa8b14.chunk.js"
+  },
+  {
+    "revision": "ad1f9536d12b04f4859e",
+    "url": "/TodosReact/static/js/runtime~main.ad1f9536.js"
+  },
+  {
+    "revision": "e093213d324ca1a7861c4c177ee52a73",
+    "url": "/TodosReact/static/media/check.e093213d.svg"
+  },
+  {
+    "revision": "bb82d6a946a5dc3c0bfc32b46614ed51",
+    "url": "/TodosReact/static/media/delete.bb82d6a9.svg"
+  },
+  {
+    "revision": "7f1be2df79b390fd14995370349a3a0b",
+    "url": "/TodosReact/index.html"
+  }
+];
